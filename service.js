519af150e301484909303acfde2531570894b4bb
Alice Robertson <robe23am@dukes.jmu.edu>
2021-10-27 18:04:48 -0400

    set up

---
+++ service.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const fs = require('fs');
+const mysql = require('mysql');
+
+const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+const connection = mysql.createConnection(credentials);
+
+const service = express();
+service.use(express.json());
+
+service.use((request, response, next) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  response.set("Access-Control-Allow-Headers", "Content-Type");
+
+  next();
+});
+
+service.options('*', (request, response) => {
+  response.set('Access-Control-Allow-Headers', 'Content-Type');
+  response.set('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  response.sendStatus(200);
+});
+
+connection.connect(error => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
+
+const port = 5001;
+service.listen(port, () => {
+  console.log(`We're live in port ${port}!`);
+});
+
+
+// curl http://localhost:5001/humans/1
+
+// curl --header 'Content-Type: application/json' \
+// --data '{"username": "user", "screen_name": "name"}' \
+// http://localhost:5001/humans
+
+// curl --request POST http://localhost:5001/follow/1/2
+
+// curl http://localhost:5001/follow/1
